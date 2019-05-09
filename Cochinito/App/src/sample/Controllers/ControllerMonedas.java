ca1b7e5f4f5aa28838d8582fce644776171164fc
h4ckerv2 <sabrisjuan@hotmail.com>
2019-05-09 18:37:28 -0500

    Agregue el sonido

--- Cochinito/App/src/sample/Controllers/ControllerMonedas.java
+++ Cochinito/App/src/sample/Controllers/ControllerMonedas.java
@@ -6,13 +6,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerMonedas implements Initializable {
-    @FXML JFXButton btnRegresar, btnSalir, btnMonedas, btnBilletes;
+    @FXML JFXButton btnRegresar, btnSalir, btnMonedas, btnBilletes,Moneda1,Moneda2,Moneda5,Moneda10,Billete20,Billete50,Billete100,Billete200;
     @FXML ImageView imv1, imv2, imv3, imv4;
 
     Main m=new Main();
@@ -27,9 +30,81 @@ public class ControllerMonedas implements Initializable {
         });
         btnMonedas.setOnAction(event -> {
             monedas();
+            Moneda1.setVisible(true);
+            Moneda2.setVisible(true);
+            Moneda5.setVisible(true);
+            Moneda10.setVisible(true);
+            Billete20.setVisible(false);
+            Billete50.setVisible(false);
+            Billete100.setVisible(false);
+            Billete200.setVisible(false);
         });
         btnBilletes.setOnAction(event -> {
             billetes();
+            Moneda1.setVisible(false);
+            Moneda2.setVisible(false);
+            Moneda5.setVisible(false);
+            Moneda10.setVisible(false);
+            Billete20.setVisible(true);
+            Billete50.setVisible(true);
+            Billete100.setVisible(true);
+            Billete200.setVisible(true);
+        });
+
+        Moneda1.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\1peso.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Moneda2.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\2pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Moneda5.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\5pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Moneda10.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\10pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Billete20.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\20pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Billete50.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\50pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Billete100.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\100pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
+        });
+
+        Billete200.setOnAction(event -> {
+            File f = new File("C:\\Users\\sabri\\Desktop\\P\\Fundamentos-Ing-Sorftware\\Cochinito\\App\\src\\sample\\Sonido\\200pesos.mp3");
+            URI u = f.toURI();
+            AudioClip sonido = new AudioClip(""+u);
+            sonido.play();
         });
     }
 
